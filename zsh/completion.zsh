68bb1fbbd5f941e986ca160a15af9c81af872335
Joonas Tiala <joonas.tiala@gmail.com>
2019-02-06 20:58:11 +0200

    Tab completion for .. (adds /)

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -12,6 +12,7 @@ unsetopt flowcontrol
 setopt auto_menu
 setopt complete_in_word
 setopt always_to_end
+setopt autoparamslash
 
 WORDCHARS=''
 
@@ -25,6 +26,7 @@ else
 	zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 fi
 
+zstyle ':completion:*' special-dirs true
 zstyle ':completion:*' list-colors ''
 zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
