53682778da9e61580e3574bb1e1a984538a20010
Joonas Tiala <joonas.tiala@gmail.com>
2019-02-07 17:31:49 +0200

    Add update script

---
+++ scripts/update.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# vim: ts=4 sw=4
+
+echo "Running update.sh"
+
+# Abort on error
+set -e
+
+# cd to the folder of the script to ensure correct paths
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+# Exit if git repository is dirty
+if ! (output=$(git status --porcelain) && [ -z "$output" ]); then
+	echo "Git repository is dirty! Commit your changes before running this update script"
+	exit 1;
+fi
+
+# Update & upgrade homebrew formulae & packages
+if [ "$(uname -s)" == "Darwin" ]; then
+	echo "Updating Homebrew formulae"
+	brew update
+	echo "Upgrading Homebrew packages"
+	brew upgrade
+fi
+
+# Update submodules
+echo "Updating submodules"
+git submodule update --remote
+
+# Commit changes if any
+if output=$(git status --porcelain) && [ -z "$output" ]; then
+	echo "Commiting changes"
+	git add .
+	git commit -m 'Updated submodules'
+	echo "Pushing to origin"
+	git push origin master
+fi
+
+echo "update.sh finished"
+
